71b4bb7f04784b5d136d0f056a78846ba50ecd61
Raymond Jacobson <ray@audius.co>
2019-05-29 14:16:45 -0700

    Minor touchups

--- js/scroll.js
+++ js/scroll.js
@@ -3,13 +3,11 @@ window.onscroll = () => onScrollFn();
 let debounced = false;
 
 function onScrollFn() {
-  window.parallax();
   window.addHeader();
-  console.log(window.height);
+  window.parallax();
   if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight) {
     if (!debounced || (window.screen.width > 600)) {
       debounced = true;
-      console.log('fire', window.innerHeight + window.scrollY, document.body.scrollHeight);
       window.fireConfetti();
       setTimeout(() => { debounced = false }, 1000);
     }
